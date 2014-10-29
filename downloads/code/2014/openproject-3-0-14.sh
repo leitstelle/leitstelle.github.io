bfe1df3bf1d9361b081c57bcfd7a36d88450c7ca
Git <hq@localhost>
2014-10-29 15:53:25 +0100

    Site updated at 2014-10-29 14:53:25 UTC

--- downloads/code/2014/openproject-3-0-14.sh
+++ downloads/code/2014/openproject-3-0-14.sh
@@ -1,75 +1,213 @@
 #!/usr/bin/env bash
 ################################################################################
-# No warranty! For nothing. Use it at your own risk.
 # Author: Frank Zisko, 2014
-# Version: 1.1.0.5
+# Version: 2.0.0.15
+# Licence: MIT
+# No warranty! For nothing. Use it at your own risk.
 #
-#
-# OpenProject 3.0.14 installation at  an uberspace account.
-# Using:
-# Ruby 2.1.2 via rbenv 0.4.0
-# Ruby 2.1.1 via uberspace
-#
+# About: OpenProject 3.0.14 installation at an uberspace account.
+# Using 
+# Ruby 2.1.2 via rbenv
+# or 
+# Ruby 2.1.2 via uberspace 
+# (Automatically choosen.)
 ################################################################################
 
 
-# Install root dir.
+### Logging ### ################################################################
+# Call this script again with logging parameters.
+if [ -z "$IS_CALLED" ]; then
+    #echo "Recall script with logging."    
+    export IS_CALLED=1
+    
+    SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/`basename "${BASH_SOURCE[0]}"`
+    LOG_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/`basename "${BASH_SOURCE[0]}"`.log
+    . ${SCRIPT_PATH} ${1} 2>&1 | tee "${LOG_PATH}"
+
+    #echo "End recursive script call."
+    # Exit here! If not, you'll go into endless recursive loop!
+    exit
+    echo "Never reach this."
+fi
+
+
+### Log ### ####################################################################
+JOB_NAME="Install OpenProject"
+echo "### Start Job: ${JOB_NAME} at $(date "+%Y-%m-%dT%H:%M:%S"). ###"
+time_begin=$(date +"%s")
+
+
+### Variables ### ##############################################################
+# You just have to adjust these four variables.
+
+# Install root directory.
 WORKSPACE=${HOME}/workspace
 
 # Projectname (used for subdomain name in your domain and uberspace domain).
-PROJECT=openproject
+PROJECT=project
 
 # Domain. 
 # Uberspace user domain (<uberuser>.<uberserver>.uberspace.de) will be automatically added.
+# It does not matter, if you don't have an own domain.
 DOMAINNAME="${PROJECT}.example.com"
 
 # Port.
 PORT=61000
 
-# Check if port is (already) unused.
-if [[ -z $(netstat -tulpen | grep ${PORT}) ]]; then
-    echo "Your choosen \$PORT is available."
-else
-    echo "Your choosen \$PORT is not available. Choose another."
-    echo "Aborting the script!"
-    echo " "
-    exit 2
+
+### Uninstall ### ##############################################################
+if [ "${1}" == "uninstall" ]; then
+    echo "Uninstallation..."
+
+    #### Services ####
+    cd ~/service/openproject-web
+    rm ~/service/openproject-web
+    svc -dx . log
+    rm -rf ~/etc/run-openproject-web
+
+    cd ~/service/openproject-worker
+    rm ~/service/openproject-worker
+    svc -dx . log
+    rm -rf ~/etc/run-openproject-worker
+  
+    #### Installation files and folder ####
+    rm ~/bin/openproject-web
+    rm ~/bin/openproject-worker
+    rm -rf /var/www/virtual/${USER}/${PROJECT}.${USER}.$(hostname -a).uberspace.de
+    # Prevent deleting ${USER}s www directory, if ${DOMAINNAME} is empty.
+    if [ -n "${DOMAINNAME}" ]; then
+        rm /var/www/virtual/${USER}/${DOMAINNAME}
+    fi
+    rm -rf ~/workspace/openproject
+    #rm -rf ~/workspace/openproject-git
+  
+    #### DB ####
+    # Drop existing DBs.
+        # Check if database is existing.
+        # Connect to DB and instantly exiting DB returns nothing.
+        # No exisitng DB returns an error.
+    if [ -z "$(mysql --user="${USER}" --password=`my_print_defaults client | grep -- --password | awk -F = '{ print $2 }'` --database="${USER}_openproject" --execute=exit 2>&1 >/dev/null)" ]; then
+        echo "Drop existing DB ${USER}_openproject."
+        mysql -e "DROP DATABASE ${USER}_openproject;"
+    fi
+
+    if [ -z "$(mysql --user="${USER}" --password=`my_print_defaults client | grep -- --password | awk -F = '{ print $2 }'` --database="${USER}_openprojects_dev" --execute=exit 2>&1 >/dev/null)" ]; then
+        echo "Drop existing DB ${USER}_openproject_dev."
+        mysql -e "DROP DATABASE ${USER}_openproject_dev;"
+    fi
+
+    if [ -z "$(mysql --user="${USER}" --password=`my_print_defaults client | grep -- --password | awk -F = '{ print $2 }'` --database="${USER}_openprojects_test" --execute=exit 2>&1 >/dev/null)" ]; then
+        echo "Drop existing DB ${USER}_openproject_test."
+        mysql -e "DROP DATABASE ${USER}_openproject_test;"
+    fi
+
+    echo "Uninstallation finished."
+    exit 0
+fi
+
+
+### Check ### ##################################################################
+# Check existing installation of openproject-web.
+if [ -f ~/bin/openproject-web ]; then
+    echo "~/bin/openproject-web exists."
+    IS_INSTALLED=1
+fi
+# Check existing installation of openproject-worker.
+if [ -f ~/bin/openproject-worker ]; then
+    echo "~/bin/openproject-worker exists."
+    IS_INSTALLED=1
+fi
+# Check termination condition.
+if [ -n "${IS_INSTALLED}" ]; then
+    echo "OpenProject files found. Maybe it is already installed. Aborting."
+    exit 1
 fi
 
 
 ### Ruby ### ###################################################################
 # You need Ruby in your $PATH (e.g. via .bashrc file)!
-# If you are not using rbenv, like me, it's will also work. You get some 'unknown command' outputs for every 'rbenv …' line. It's not nice, but it works for both: Uberspace Ruby and rbenv Ruby.
-rbenv install -s 2.1.2
-rbenv rehash
+
+# Check if port is (already) unused.
+if [ -z "$(netstat -tulpen | grep ${PORT})" ]; then
+    echo "Your choosen \$PORT:${PORT} is available."
+else
+    echo "Your choosen \$PORT:${PORT} is not available. Choose another."
+    echo "Aborting the script."
+    exit 2
+fi
+
+# Check if rbenv ruby is used. Important for 'gem install …' commnd.
+if [ -z "$(command -v rbenv)" ]; then
+    echo "'rbenv' command not found."
+    IS_RBENV=0
+else
+    echo "'rbenv' command found."
+    IS_RBENV=1
+    echo "rbenv version:"
+    rbenv --version
+fi
+
+# Check if ruby is available.
+if [ -z "$(command -v ruby)" ]; then
+    echo "'ruby' command not found."
+    RUBY_FOUND=false
+else
+    RUBY_FOUND=true
+    echo "'ruby' command found."
+fi
+# Check if gem is available.
+if [ -z "$(command -v gem)" ]; then
+    echo "'gem' command not found."
+    GEM_FOUND=false
+else
+    GEM_FOUND=true
+    echo "'gem' command found."
+fi
+# Check termination condition.
+if [[ ( ${RUBY_FOUND} == true ) && ( ${GEM_FOUND} == true ) ]]; then
+    echo " "
+else
+    echo "Ruby is not complete. Aborting."
+    exit 3
+fi
+
+# Install rbenv ruby version.
+if [ "$IS_RBENV" == 1 ]; then
+    echo "'rbenv install -s 2.1.2'."
+    rbenv install -s 2.1.2
+    rbenv rehash 
+else
+    echo "PATH:"
+    echo $PATH
+fi
 
 
 ### Download ### ############################################################
 mkdir -p ${WORKSPACE}; cd ${WORKSPACE}
 
 if [ -d openproject-git ]; then
-	echo "Source directory openproject exists. Updateing and copying data from there."
-	cd ${WORKSPACE}/openproject-git
-	git pull
-	#git checkout stable
-	git checkout v3.0.14
+    echo "Source directory openproject exists. Updateing and copying data from there."
+    cd ${WORKSPACE}/openproject-git
+    git pull
+    #git checkout stable
+    git checkout v3.0.14
 else
-    	git clone https://github.com/opf/openproject.git ${WORKSPACE}/openproject-git
-	cd ${WORKSPACE}/openproject-git
-	#git checkout stable
-	git checkout v3.0.14
+    git clone https://github.com/opf/openproject.git ${WORKSPACE}/openproject-git
+    cd ${WORKSPACE}/openproject-git
+    #git checkout stable
+    git checkout v3.0.14
 fi
 
 
 ### Prepare ### ################################################################
 cd ${WORKSPACE}
+# With this all dot-leading files and folders will not be copied.
 rm -Rf openproject && mkdir -p openproject
 cp -R openproject-git/* openproject/
 
 
 ### Config ### #################################################################
 cd ${WORKSPACE}/openproject
-rbenv local 2.1.2
 
 #### configuration.yml ####
 cat > config/configuration.yml <<__EOF__
@@ -131,30 +269,57 @@ __EOF__
 
 ### Gems ### ###################################################################
 cd ${WORKSPACE}/openproject
+
+if [ "$IS_RBENV" == "1" ]; then
+    echo "'rbenv local 2.1.2'."
+    rbenv local 2.1.2
+fi
+
+# Uberspace ruby installs into ~/.gem. rbenv ruby into project folder.
+if [ "$IS_RBENV" == "1" ]; then
+    gem install bundler
+    rbenv rehash
+else
+    gem install --user bundler
+fi
+
 cat > Gemfile.local <<__EOF__
-#gem 'unicorn'
 gem 'rails_12factor'
 __EOF__
 
-gem install bundler
-rbenv rehash
-
-# For ruby from system:
-#bundle install --path ~/.gem --without postgres:sqlite:test
-# For ruby from local user rbenv:
-bundle install --path vendor/bundle --without postgres:sqlite:test:development
-	# "--path vendor/bundle" will install gems in the project folder.
+# This works for Uberspace ruby and rbenv ruby.
+# '--path vendor/bundle' will install gems in the project folder. 
+# Normally on Uberspace you choose '--path ~/.gem'.
+bundle install --path vendor/bundle --without postgres:sqlite:development:test
 
 
 ### Setup ### ##################################################################
 cd ${WORKSPACE}/openproject
+
 bundle exec rake generate_secret_token
 
 #### DB ####
-mysql -e "DROP DATABASE ${USER}_openproject;"
-#mysql -e "DROP DATABASE ${USER}_openproject_dev;"
-#mysql -e "DROP DATABASE ${USER}_openproject_test;"
 
+# Drop existing DBs.
+    # Check if database is existing.
+    # Connect to DB and instantly exiting DB returns nothing.
+    # No exisitng DB returns an error.
+if [ -z "$(mysql --user="${USER}" --password=`my_print_defaults client | grep -- --password | awk -F = '{ print $2 }'` --database="${USER}_openproject" --execute=exit 2>&1 >/dev/null)" ]; then
+    echo "Drop existing DB ${USER}_openproject."
+    mysql -e "DROP DATABASE ${USER}_openproject;"
+fi
+
+if [ -z "$(mysql --user="${USER}" --password=`my_print_defaults client | grep -- --password | awk -F = '{ print $2 }'` --database="${USER}_openprojects_dev" --execute=exit 2>&1 >/dev/null)" ]; then
+    echo "Drop existing DB ${USER}_openproject_dev."
+    mysql -e "DROP DATABASE ${USER}_openproject_dev;"
+fi
+
+if [ -z "$(mysql --user="${USER}" --password=`my_print_defaults client | grep -- --password | awk -F = '{ print $2 }'` --database="${USER}_openprojects_test" --execute=exit 2>&1 >/dev/null)" ]; then
+    echo "Drop existing DB ${USER}_openproject_test."
+    mysql -e "DROP DATABASE ${USER}_openproject_test;"
+fi
+
+# Create new DB.
 mysql -e "CREATE DATABASE ${USER}_openproject CHARACTER SET utf8"
 #mysql -e "CREATE DATABASE ${USER}_openproject_dev CHARACTER SET utf8"
 #mysql -e "CREATE DATABASE ${USER}_openproject_test CHARACTER SET utf8"
@@ -203,7 +368,9 @@ uberspace-setup-service openproject-worker ~/bin/openproject-worker
 
 ### Rewrite Proxy ### ##########################################################
 mkdir -p /var/www/virtual/${USER}/${PROJECT}.${USER}.$(hostname -a).uberspace.de
-ln -s /var/www/virtual/${USER}/${PROJECT}.${USER}.$(hostname -a).uberspace.de /var/www/virtual/${USER}/${DOMAINNAME}
+if [ -n "${DOMAINNAME}" ]; then
+    ln -s /var/www/virtual/${USER}/${PROJECT}.${USER}.$(hostname -a).uberspace.de /var/www/virtual/${USER}/${DOMAINNAME}
+fi
 
 cd /var/www/virtual/${USER}/${PROJECT}.${USER}.$(hostname -a).uberspace.de
 
@@ -217,36 +384,34 @@ RewriteRule (.*) http://localhost:${PORT}/\$1 [P]
 __EOF__
 
 
-### Start/Stop ### #############################################################
+### Info ### ###################################################################
+echo " "
+echo " "
 echo "Restart OpenProject services:"
-echo "svc -du ~/service/openproject-web"
-echo "svc -du ~/service/openproject-worker"
+echo "    svc -du ~/service/openproject-web"
+echo "    svc -du ~/service/openproject-worker"
 
 
-### Logs ### ###################################################################
+### Info ### ###################################################################
+echo " "
+echo " "
 echo "Logs will be written in:"
-echo "~/service/openproject-web/log/main/current"
-echo "~/service/openproject-worker/log/main/current"
+echo "    ~/service/openproject-web/log/main/current"
+echo "    ~/service/openproject-worker/log/main/current"
 
 
-### Uninstall ### ##############################################################
+### Info ### ###################################################################
+echo " "
+echo " "
+echo "For uninstallation call:"
+echo "    $(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/`basename "${BASH_SOURCE[0]}"` uninstall"
 
-#### Services ####
-#  cd ~/service/openproject-web
-#  rm ~/service/openproject-web
-#  svc -dx . log
-#  rm -rf ~/etc/run-openproject-web
 
-#  cd ~/service/openproject-worker
-#  rm ~/service/openproject-worker
-#  svc -dx . log
-#  rm -rf ~/etc/run-openproject-worker
-
-#### Installation files and folder ####
-#rm ~/bin/openproject-web
-#rm ~/bin/openproject-worker
-#rm -rf /var/www/virtual/${USER}/${PROJECT}.${USER}.$(hostname -a).uberspace.de
-#rm /var/www/virtual/${USER}/${DOMAINNAME}
-#rm -rf ~/workspace/openproject
-###rm -rf ~/workspace/openproject-git
+### Log ### ####################################################################
+echo " "
+echo " "
+time_end=$(date +"%s")
+time_diff=$((${time_end}-${time_begin}))
+echo "### Finished Job: ${JOB_NAME} at $(date "+%Y-%m-%dT%H:%M:%S"). ###"
+echo "    $((${time_diff} / 60)) minutes and $((${time_diff} % 60)) seconds elapsed."
 
